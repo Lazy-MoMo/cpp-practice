0ca45a831dadd1949f0f8f5f2c2e404e25cfef48
Lazy-MoMo <abhisekhjoshi74@gmail.com>
2024-05-27 08:19:26 +0545

    a

---
+++ c2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+//working with numbers
+int main()
+{
+  double num = 5.76;
+  double num2 = 2.01;
+  cout << 5 * 7 <<endl;
+  cout << round(num)<<endl; //rounds to the nearest integer
+  cout << ceil(num2)<<endl; //rounds up to the interger
+  cout << pow(2,2)<<endl; //digit , power of digit
+  cout << sqrt(36)<<endl; //square root
+  cout <<floor(num2)<<endl; //round down
+  cout << fmax(3,10) <<endl; //gives bigger num
+  cout << fmin(3,10) <<endl; //gives smaller num
+}
